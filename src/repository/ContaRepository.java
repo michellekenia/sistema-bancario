eef2e633855258e3cdf33400dc19d1e56c3e3532
mih <keniamsoaress@gmail.com>
2025-02-20 16:54:49 -0300

    refactor

--- src/repository/ContaRepository.java
+++ src/repository/ContaRepository.java
@@ -7,17 +7,27 @@ import java.util.List;
 
 public class ContaRepository {
 
-    private List <Conta> contas = new ArrayList<>();
+    private List<Conta> contas = new ArrayList<>();
 
-   public void addConta(Conta conta ) {
-       contas.add(conta);
-   }
+    public void addConta(Conta conta) {
+        contas.add(conta);
+    }
 
-   public Conta buscarConta(String numeroConta){
-       return contas.stream()
-               .filter(conta -> conta.getNumeroConta().equals(numeroConta))
-               .findFirst()
-               .orElse(null);
-   }
+    public Conta buscarConta(String numeroConta) {
+        for (Conta conta : contas) {
+            if (conta.getNumeroConta().equals(numeroConta)) {
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public List<Conta> listarContas() {
+        return contas;
+    }
+
+    public void removerConta(String numeroConta) {
+        contas.removeIf(conta -> conta.getNumeroConta().equals(numeroConta));
+    }
 
 }
