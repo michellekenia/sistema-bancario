fad1f15102cd22cf66b0a5d65e43293aa1255bd8
mih <keniamsoaress@gmail.com>
2025-02-26 12:32:53 -0300

    atualizar-atributos

--- src/model/Transacao.java
+++ src/model/Transacao.java
@@ -4,18 +4,24 @@ import java.time.LocalDateTime;
 
 public class Transacao {
 
+    private String id;
     private String tipo;
     private double valor;
     private Conta contaOrigem;
     private Conta contaDestino;
     private LocalDateTime dataHora;
 
-    public Transacao(String tipo, double valor, Conta contaOrigem, Conta contaDestino, LocalDateTime dataHora) {
+    public Transacao(String id, String tipo, double valor, Conta contaOrigem, Conta contaDestino, LocalDateTime dataHora) {
+        this.id = id;
         this.tipo = tipo;
         this.valor = valor;
         this.contaOrigem = contaOrigem;
         this.contaDestino = contaDestino;
         this.dataHora = dataHora;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getTipo() {
@@ -37,5 +43,4 @@ public class Transacao {
     public LocalDateTime getDataHora() {
         return dataHora;
     }
-
 }
